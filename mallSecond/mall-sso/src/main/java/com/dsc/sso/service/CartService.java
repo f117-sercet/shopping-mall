4d7e7420bb91ec5146aa75eb075be89590913734
f117-serect <15159885771@163.com>
2020-10-10 19:56:51 +0800

    mallSecond

--- mallSecond/mall-sso/src/main/java/com/dsc/sso/service/CartService.java
+++ mallSecond/mall-sso/src/main/java/com/dsc/sso/service/CartService.java
@@ -24,4 +24,38 @@ public interface CartService {
      * @return
      */
   List<CartProduct> getCartList(long userId);
+
+    /**
+     * 更新
+     * @param userId
+     * @param itemId
+     * @param num
+     * @param checked
+     * @return
+     */
+  int updateCartNum(long userId,long itemId,int num,String checked);
+
+    /**
+     * 全选反选
+     * @param userId
+     * @param checkd
+     * @return
+     */
+  int checkAll(long userId,String checkd);
+
+    /**
+     * 删除多个
+     *
+     * @param userId
+     * @param itemId
+     * @return
+     */
+   int deleteCartItem(long userId,long itemId);
+
+    /**
+     * 删除全勾选的
+     * @param userId
+     * @return
+     */
+   int delChecked(long userId);
 }
