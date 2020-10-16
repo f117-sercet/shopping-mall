e801448f761383b74c58dbcaf5ec2abc5d915586
f117-serect <15159885771@163.com>
2020-10-16 21:23:06 +0800

    mallSecond

---
+++ mallSecond/mall-sso/mall-sso-service/src/main/java/com/dsc/sso/service/impl/LoginServiceImpl.java
@@ -0,0 +1,73 @@
+package com.dsc.sso.service.impl;
+
+import com.dsc.common.jedis.JedisClient;
+import com.dsc.mall.manager.dto.DtoUtil;
+import com.dsc.mall.manager.dto.front.Member;
+import com.dsc.mall.manager.mapper.TbMemberMapper;
+import com.dsc.mall.manager.pojo.TbMember;
+import com.dsc.mall.manager.pojo.TbMemberExample;
+import com.dsc.sso.service.LoginService;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.DigestUtils;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * 登录实现类
+ * @author 60221
+ */
+public class LoginServiceImpl implements LoginService {
+    @Autowired
+    private TbMemberMapper tbMemberMapper;
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("1800")
+    private Integer SESSION_EXPIRE;
+
+    @Override
+    public Member userLogin(String username, String password) {
+        TbMemberExample example = new TbMemberExample();
+        TbMemberExample.Criteria criteria = example.createCriteria();
+        criteria.andStateEqualTo(1);
+        criteria.andUsernameEqualTo(username);
+        List<TbMember> list = tbMemberMapper.selectByExample(example);
+        if (list == null || list.size() == 0) {
+            Member member = new Member();
+            member.setState(0);
+            member.setMessage("用户名密码错误");
+
+            return member;
+        }
+        TbMember tbMember=list.get(0);
+        //md5加密
+        if (!DigestUtils.md5DigestAsHex(password.getBytes()).equals(tbMember.getPassword())){
+            Member member=new Member();
+            member.setState(0);
+            member.setMessage("用户名或密码错误");
+            member.setState(1);
+        }
+        String token = UUID.randomUUID().toString();
+        Member member= DtoUtil.TbMemer2Member(tbMember);
+        member.setToken(token);
+        member.setState(1);
+        // 用户信息写入redis：key："SESSION:token" value："user"
+        jedisClient.set("SESSION:" + token, new Gson().toJson(member));
+        jedisClient.expire("SESSION:" + token, SESSION_EXPIRE);
+
+        return member;
+    }
+
+
+    @Override
+    public Member getUserByToken(String token) {
+        return null;
+    }
+
+    @Override
+    public int logout(String token) {
+        return 0;
+    }
+}
