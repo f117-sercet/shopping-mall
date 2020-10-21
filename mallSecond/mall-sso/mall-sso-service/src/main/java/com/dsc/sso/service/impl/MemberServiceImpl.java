f7795906031bb89dd0644e9a72899b0d9f9ad4c1
f117-serect <15159885771@163.com>
2020-10-21 22:19:09 +0800

    mallSecond

--- mallSecond/mall-sso/mall-sso-service/src/main/java/com/dsc/sso/service/impl/MemberServiceImpl.java
+++ mallSecond/mall-sso/mall-sso-service/src/main/java/com/dsc/sso/service/impl/MemberServiceImpl.java
@@ -1,10 +1,13 @@
 package com.dsc.sso.service.impl;
 
 import com.dsc.common.jedis.JedisClient;
+import com.dsc.common.utils.QiniuUtil;
 import com.dsc.mall.manager.mapper.TbMemberMapper;
+import com.dsc.mall.manager.pojo.TbMember;
 import com.dsc.sso.service.LoginService;
 import com.dsc.sso.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -23,7 +26,12 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public String imageUpload(Long userId, String token, String imgData) {
 
-        //
+        //过滤data:URL
+        String base64= QiniuUtil.base64Data(imgData);
+        String imgPath=QiniuUtil.qiniuUpload(base64);
+
+        TbMember tbMember=tbMemberMapper.selectByPrimaryKey(userId);
+
         return null;
     }
 }
