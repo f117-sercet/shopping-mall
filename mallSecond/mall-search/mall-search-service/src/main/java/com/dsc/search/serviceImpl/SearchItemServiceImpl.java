d9d3ee81dfec29897027e17c4b8d2a5413db38fa
f117-serect <15159885771@163.com>
2020-11-05 20:15:15 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	mallSecond/.idea/workspace.xml

--- mallSecond/mall-search/mall-search-service/src/main/java/com/dsc/search/serviceImpl/SearchItemServiceImpl.java
+++ mallSecond/mall-search/mall-search-service/src/main/java/com/dsc/search/serviceImpl/SearchItemServiceImpl.java
@@ -1,7 +1,19 @@
 package com.dsc.search.serviceImpl;
 
 import com.dsc.mall.manager.dto.EsInfo;
+import com.dsc.search.mapper.ItemMapper;
 import com.dsc.search.search.SearchItemService;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.Transport;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.InetAddress;
 
 /**
  * 实现类1
@@ -9,9 +21,34 @@ import com.dsc.search.search.SearchItemService;
  */
 public class SearchItemServiceImpl implements SearchItemService
 {
+    private  final static Logger log = LoggerFactory.getLogger(SearchItemServiceImpl.class);
 
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @Value("127.0.0.1")
+    private String ES_CONNECT_IP;
+
+    @Value("$9200")
+    private String ES_NODE_CLIENT_PORT;
+
+    @Value("elasticsearch")
+    private String ES_CLUSTER_NAME;
+
+    @Value("$item")
+    private String ITEM_INDEX;
+
+    @Value("itemList")
+    private String ITEM_TYPE;
     @Override
     public int importAllItems() {
+
+        try {
+            Settings settings= Settings.builder().put("cluster.name",ES_CLUSTER_NAME).build();
+            TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName(ES_CONNECT_IP),93000));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
--- mallSecond/mall-search/mall-search-service/src/main/java/com/dsc/search/serviceImpl/SearchServiceImpl.java
+++ mallSecond/mall-search/mall-search-service/src/main/java/com/dsc/search/serviceImpl/SearchServiceImpl.java
@@ -1,4 +1,20 @@
 package com.dsc.search.serviceImpl;
 
-public class SearchServiceImpl {
+import com.dsc.mall.manager.dto.front.SearchResult;
+import com.dsc.search.search.SearchService;
+
+/**
+ * Search实现类
+ * @author dsc
+ */
+public class SearchServiceImpl implements SearchService {
+    @Override
+    public SearchResult search(String keyword, int page, int size, String sort, int priceGt, int priceLte) {
+        return null;
+    }
+
+    @Override
+    public String quickSearch(String key) {
+        return null;
+    }
 }
