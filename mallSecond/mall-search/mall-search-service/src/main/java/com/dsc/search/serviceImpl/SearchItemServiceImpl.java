867ed0b2ee8d8796b9a863b115e5e4ea53e42ccb
f117-serect <15159885771@163.com>
2020-11-07 13:07:04 +0800

    branch'origin/master'

--- mallSecond/mall-search/mall-search-service/src/main/java/com/dsc/search/serviceImpl/SearchItemServiceImpl.java
+++ mallSecond/mall-search/mall-search-service/src/main/java/com/dsc/search/serviceImpl/SearchItemServiceImpl.java
@@ -1,8 +1,16 @@
 package com.dsc.search.serviceImpl;
 
+import com.dsc.common.execetion.MallException;
+import com.dsc.common.utils.HttpUtil;
+import com.dsc.mall.manager.dto.EsCount;
 import com.dsc.mall.manager.dto.EsInfo;
+import com.dsc.mall.manager.dto.front.SearchItem;
 import com.dsc.search.mapper.ItemMapper;
 import com.dsc.search.search.SearchItemService;
+import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
@@ -14,6 +22,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.net.InetAddress;
+import java.util.List;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 /**
  * 实现类1
@@ -46,14 +57,71 @@ public class SearchItemServiceImpl implements SearchItemService
         try {
             Settings settings= Settings.builder().put("cluster.name",ES_CLUSTER_NAME).build();
             TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName(ES_CONNECT_IP),93000));
+
+            //批量添加
+            BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+            //查询商品列表
+            List<SearchItem> itemList = itemMapper.getItemList();
+            for (SearchItem searchItem: itemList){
+                String imag = searchItem.getProductImageBig();
+                if (imag != null && !"".equals(imag)){
+                    String[] strings = imag.split(",");
+                    imag=strings[0];
+                }else {
+                    imag="";
+                }
+                searchItem.setProductImageBig(imag);
+                bulkRequest.add(client.prepareIndex(ITEM_INDEX, ITEM_TYPE, String.valueOf(searchItem.getProductId()))
+                        .setSource(jsonBuilder()
+                                .startObject()
+                                .field("productId", searchItem.getProductId())
+                                .field("salePrice", searchItem.getSalePrice())
+                                .field("productName", searchItem.getProductName())
+                                .field("subTitle", searchItem.getSubTitle())
+                                .field("productImageBig", searchItem.getProductImageBig())
+                                .field("categoryName", searchItem.getCategoryName())
+                                .field("cid", searchItem.getCid())
+                                .endObject()
+                        )
+                );
+            }
+
+            BulkResponse bulkResponse = bulkRequest.get();
+
+            log.info("更新索引成功");
+
+            client.close();
+
         } catch (Exception e) {
             e.printStackTrace();
+            throw new MallException("导入ES索引库出错，请再次尝试");
         }
-        return 0;
+        return 1;
     }
 
     @Override
     public EsInfo getEsInfo() {
-        return null;
+        String healthUrl="http://"+ES_CONNECT_IP+":"+ES_NODE_CLIENT_PORT+"/_cluster/health";
+        String countUrl="http://"+ES_CONNECT_IP+":"+ES_NODE_CLIENT_PORT+"/_count";
+        String healthResult= HttpUtil.sendGet(healthUrl);
+        String countResult=HttpUtil.sendGet(countUrl);
+        if(StringUtils.isBlank(healthResult)||StringUtils.isBlank(countResult)){
+            throw new MallException("连接集群失败，请检查ES运行状态");
+        }
+        EsInfo esInfo=new EsInfo();
+        EsCount esCount=new EsCount();
+        try {
+            esInfo=new Gson().fromJson(healthResult,EsInfo.class);
+            esCount=new Gson().fromJson(countResult,EsCount.class);
+            esInfo.setCount(esCount.getCount());
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new MallException("获取ES信息出错");
+        }
+
+        return esInfo;
+
+
     }
 }
