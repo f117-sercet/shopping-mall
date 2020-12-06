c17edf08b7ad3b9172cda25c2abb3f5409d82e1b
f117-serect <15159885771@163.com>
2020-12-06 13:27:55 +0800

    更新Controller方法

---
+++ mallSecond/mall-front-web/src/main/java/com/dsc/front/controller/AddressController.java
@@ -0,0 +1,34 @@
+package com.dsc.front.controller;
+
+import com.dsc.common.pojo.Result;
+import com.dsc.common.utils.ResultUtil;
+import com.dsc.mall.manager.pojo.TbAddress;
+import com.dsc.sso.service.AddressService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author  dsc
+ *
+ */
+@RestController
+@Api(description = "收货地址")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @RequestMapping(value = "/member/addressList",method = RequestMethod.POST)
+    @ApiOperation(value = "获得收货地址")
+    public Result<List<TbAddress>> addressList(@RequestBody TbAddress tbAddress){
+        List<TbAddress> list=addressService.getAddressList(tbAddress.getUserId());
+        return new ResultUtil<List<TbAddress>>().setData(list);
+    }
+}
