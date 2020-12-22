97a5c69d6bfd32d79276f42bac67bbb2cfd05337
f117-serect <15159885771@163.com>
2020-12-22 19:00:33 +0800

    订单controller

--- mallSecond/mall-front-web/src/main/java/com/dsc/front/controller/OrderController.java
+++ mallSecond/mall-front-web/src/main/java/com/dsc/front/controller/OrderController.java
@@ -3,15 +3,14 @@ package com.dsc.front.controller;
 import com.dsc.common.pojo.Result;
 import com.dsc.common.utils.ResultUtil;
 import com.dsc.mall.manager.dto.front.Order;
+import com.dsc.mall.manager.dto.front.OrderInfo;
 import com.dsc.mall.manager.dto.front.PageOrder;
+import com.dsc.mall.manager.pojo.TbThanks;
 import com.dsc.sso.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author  dsc
@@ -39,6 +38,37 @@ public class OrderController {
 
         Order order = orderService.getOrder(Long.valueOf(orderId));
         return new ResultUtil<Order>().setData(order);
+    }
+    @RequestMapping(value = "/member/addOrder",method = RequestMethod.POST)
+    @ApiOperation(value = "创建订单")
+    public Result<Object> addOrder(@RequestBody OrderInfo orderInfo){
+
+        Long orderId=orderService.createOrder(orderInfo);
+        return new ResultUtil<Object>().setData(orderId.toString());
+    }
+
+    @RequestMapping(value = "/member/cancelOrder",method = RequestMethod.POST)
+    @ApiOperation(value = "取消订单")
+    public Result<Object> cancelOrder(@RequestBody Order order){
+
+        int result=orderService.cancelOrder(order.getOrderId());
+        return new ResultUtil<Object>().setData(result);
+    }
+
+    @RequestMapping(value = "/member/delOrder",method = RequestMethod.GET)
+    @ApiOperation(value = "删除订单")
+    public Result<Object> delOrder(String orderId){
+
+        int result=orderService.delOrder(Long.valueOf(orderId));
+        return new ResultUtil<Object>().setData(result);
+    }
+
+    @RequestMapping(value = "/member/payOrder",method = RequestMethod.POST)
+    @ApiOperation(value = "支付订单")
+    public Result<Object> payOrder(@RequestBody TbThanks tbThanks){
+
+        int result=orderService.payOrder(tbThanks);
+        return new ResultUtil<Object>().setData(result);
     }
 
 }
